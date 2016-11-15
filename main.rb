9117cde1c640e47fc162d9f63b9f2554fe8d0e49
James Gifford <james@armyofminions.com>
2016-11-15 11:19:09 -0500

    Added in a little server that handles UDP and playing the mp3

--- main.rb
+++ main.rb
@@ -1,7 +1,25 @@
 require 'socket'
+require 'pry'
 s = UDPSocket.new
 s.bind(nil, 1234)
-5.times do
+$pid = nil
+loop do
   text, sender = s.recvfrom(16)
-  puts text
+  case text
+  when "play"
+    puts "playing horn"
+    if $pid
+      puts "still playing horn"
+    else
+      $pid = fork{ exec 'mpg123', '--loop', '-1', './sound/klaxon.mp3' }
+    end
+  when "stop"
+    if $pid
+      puts "killing"
+      Process.kill("HUP", $pid)
+      puts "killed"
+    else
+      puts "horn not sounding"
+    end
+  end
 end
