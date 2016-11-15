c6ec0b5f3d9bb739997482176b6fa017412c0581
James Gifford <james@armyofminions.com>
2016-11-15 16:38:30 -0500

    hotfix

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 from gpiozero import Button
 from threading import Thread
 import socket
+import sys
 
 key = Button(4)
 btn = Button(17)
 UDP_IP = "192.168.3.56"
 UDP_PORT = 1234
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 
 def function1():
     while True:
         btn.wait_for_press()
-        sock.sendto("classic_destroy", (UDP_IP, UDP_PORT))
+        sock.sendto(bytes("classic_destroy", "utf-8"), (UDP_IP, UDP_PORT))
         print("hello world, from btn")
         btn.wait_for_release()
-        sock.sendto("classic_destroy", (UDP_IP, UDP_PORT))
+        sock.sendto(bytes("classic_destroy", "utf-8"), (UDP_IP, UDP_PORT))
         print("goodbye world from btn")
 
 def function2():
     while True:
         key.wait_for_press()
-        sock.sendto("start_horn", (UDP_IP, UDP_PORT))
+        sock.sendto(bytes("start_horn", "utf-8"), (UDP_IP, UDP_PORT))
         print("hello world from the key")
         key.wait_for_release()
-        sock.sendto("stop_horn", (UDP_IP, UDP_PORT))
+        sock.sendto(bytes("stop_horn", "utf-8"), (UDP_IP, UDP_PORT))
         print("goodbye world from the key")
         
 function1 = Thread(target=function1)
